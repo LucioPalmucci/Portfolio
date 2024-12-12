e6ba7d1ae05aba252870e7a86770c851a5405c4d
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-12-12 11:56:22 -0300

    Introducción

--- src/Components/Introduction/Intro.jsx
+++ src/Components/Introduction/Intro.jsx
@@ -7,7 +7,26 @@ function Intro() {
         </div>
         <div className='mt-4 text-start w-1/3'>
             <h1 className='text-2xl'>Hi, I'm Lucio Palmucci</h1>
-            <p>Web Developer</p>
+            <p className=''>Web Developer</p>
+            <br/>
+            <p>Estudiante de 2º año de Ingeniería en Informática con
+            experiencia en voluntariado, buscando mi primera
+            oportunidad laboral en sistemas. Con muchas ganas de
+            contribuir en proyectos de tecnología, aportando mis
+            habilidades en programación o el uso de herramientas
+            del software.
+            </p>
+            <br/>
+            <p>Tecnologias que manejo:</p>
+            <p align="left" className='flex space-x-1'>
+              <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5"/>
+              <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="CSS3"/>
+              <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript"/>
+              <img src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black" alt="React"/>
+              <img src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white" alt="Node.js"/>
+              <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python"/>
+              <img src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" alt="Git"/>
+            </p>
         </div>
     </div>
   );
