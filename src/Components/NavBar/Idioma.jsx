e992ebb3bb8435e59327e35bba397b636f7237e6
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-12-14 13:32:33 -0300

    Centrar navbar

---
+++ src/Components/NavBar/Idioma.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../Context/LanguageContext';
+export default function Idioma() {
+    const { isEnglish, toggleLanguage } = useContext(LanguageContext);
+
+    return (
+        <div className='text-gray-100'>
+            <button
+                onClick={() => toggleLanguage()}
+                className={`py-2 px-4 text-sm rounded-l-2xl border transition duration-300 ${isEnglish ? 'bg-gray-700' : 'hover:bg-gray-700'}`}
+            >
+                English
+            </button>
+            <button
+                onClick={() => toggleLanguage()}
+                className={`py-2 px-4 text-sm rounded-r-2xl border transition duration-300 ${!isEnglish ? 'bg-gray-700' : 'hover:bg-gray-700'}`}
+            >
+                Español
+            </button>
+        </div>
+    );
+}
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,30 @@
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useContext } from 'react';
+import { LanguageContext } from '../../Context/LanguageContext';
+import Idioma from './Idioma';
 import "./NavBar.css";
 
-function NavBar(){
-    return(
+function NavBar() {
+    const { isEnglish } = useContext(LanguageContext);
+    const Inicio = isEnglish ? 'Home' : 'Inicio';
+    const Proyectos = isEnglish ? 'Projects' : 'Proyectos';
+    const Experiencia = isEnglish ? 'Experience' : 'Experiencia';
+
+    return (
         <nav className='flex justify-between p-5 font-mono text-white'>
-            <h1 className='tracking-tighter'>Lucio Palmucci</h1>
-            <div className="flex justify-between">
-                <div className="flex space-x-10">
-                    <a href="#Home" className=" hover:text-gray-200">Inicio</a>
-                    <a href="#Proyectos" className=" hover:text-gray-200">Proyectos</a>
-                    <a href="#Experiencia" className=" hover:text-gray-200">Experiencia</a>
-                </div>
+            <Idioma />
+            <div className="flex space-x-10 justify-between mr-14">
+                <a href="#Home" className=" hover:text-gray-200">{Inicio}</a>
+                <a href="#Proyectos" className=" hover:text-gray-200">{Proyectos}</a>
+                <a href="#Experiencia" className=" hover:text-gray-200">{Experiencia}</a>
             </div>
             <div id="Redes" className=' space-x-3 justify-end flex'>
                 <a href='https://github.com/LucioPalmucci' target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faGithub} size="2x" className="icon text-white active:text-gray-600 icon-hover animate-slideDown" />
+                    <FontAwesomeIcon icon={faGithub} size="2x" className="icon text-white active:text-gray-600 icon-hover animate-slideDown" />
                 </a>
                 <a href='https://www.linkedin.com/in/lucio-palmucci' target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} size='lg' className="icon text-white active:text-gray-600 icon-hover animate-slideDown" />
+                    <FontAwesomeIcon icon={faLinkedin} size='lg' className="icon text-white active:text-gray-600 icon-hover animate-slideDown" />
                 </a>
             </div>
         </nav>
