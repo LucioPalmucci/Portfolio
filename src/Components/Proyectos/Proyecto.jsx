40615eec37ca6526778a3f29467e14f4dd84b809
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-12-14 13:12:12 -0300

    Proyecto 2.0

--- src/Components/Proyectos/Proyecto.jsx
+++ src/Components/Proyectos/Proyecto.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../Context/LanguageContext';
 
 function Proyecto({ title, description, imageUrl, repoUrl }) {
-  return (
-    <div className="bg-gray-800 p-4 rounded-lg shadow-lg m-4 w-40">
-      <img src={imageUrl} alt={title} width={300} height={150} className="rounded-lg" />
-      <div className="p-4">
-        <h2 className="text-xl font-bold text-white">{title}</h2>
-        <p className="text-gray-400 mt-2">{description}</p>
-        <a
-          href={repoUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
-        >
-          Ver en GitHub
-        </a>
-      </div>
-    </div>
-  );
+    const { isEnglish } = useContext(LanguageContext);
+
+    return (
+        <div className="bg-gray-900 p-4 rounded-lg shadow-lg m-4 w-40">
+            <img src={imageUrl} alt={title} width={300} height={150} className="rounded-lg" />
+            <div className="p-4">
+                <h2 className="text-xl font-bold text-white">{title}</h2>
+                <p className="text-gray-400 mt-2">{description}</p>
+                <a
+                    href={repoUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-4 inline-block bg-gray-500 text-white py-2 px-4 rounded hover:bg-blue-800 transition duration-300"
+                >
+                    {isEnglish ? 'View Repository' : 'Ver Repositorio'}
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default Proyecto;
--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -1,65 +1,69 @@
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import exploraventuraImg from '../../assets/Exploraventura.png';
+import { LanguageContext } from '../../Context/LanguageContext';
 import Proyecto from './Proyecto';
 
 function Proyectos() {
-
+    const { isEnglish } = useContext(LanguageContext);
+    
     const [showButton, setShowButton] = useState(false);
-        useEffect(() => {
-            const handleScroll = () => {
+    useEffect(() => {
+        const handleScroll = () => {
             if (window.scrollY > 300) {
                 setShowButton(true);
             } else {
                 setShowButton(false);
             }
-            };
-    
-            window.addEventListener('scroll', handleScroll);
-            return () => {
-            window.removeEventListener('scroll', handleScroll);
-            };
-        }, []);
-    
-        const scrollToTop = () => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
         };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     
     const proyectos = [
         {
-        title: 'Explora Aventura',
-        description: 'Clon de la pagina Oficial de exploraventura.com.ar',
-        imageUrl: exploraventuraImg,
-        repoUrl: 'https://github.com/LucioPalmucci/ExploraAventura'
+            title: 'Explora Aventura',
+            description: isEnglish ? 'Clone of the official page of exploraventura.com.ar' : 'Clon de la pagina Oficial de exploraventura.com.ar',
+            imageUrl: exploraventuraImg,
+            repoUrl: 'https://github.com/LucioPalmucci/ExploraAventura',
         },
     ];
+    
 
     return (
         <div id="Proyectos" className="justify-center items-center flex flex-col font-mono text-white mt-20">
             <div className='mt-4 text-start w-40 animate-slideDown25'>
                 <h1 className="text-2xl">
-                <FontAwesomeIcon icon={faCode} className="text-2xl"/> Proyectos
+                    <FontAwesomeIcon icon={faCode} className="text-2xl" /> {isEnglish ? 'Projects:' : 'Proyectos:'}
                 </h1>
             </div>
             <div className="flex flex-wrap justify-start w-40 animate-slideRight">
                 {proyectos.map((proyecto, index) => (
-                <Proyecto
-                    key={index}
-                    title={proyecto.title}
-                    description={proyecto.description}
-                    imageUrl={proyecto.imageUrl}
-                    repoUrl={proyecto.repoUrl}
-                />
+                    <Proyecto
+                        key={index}
+                        title={proyecto.title}
+                        description={proyecto.description}
+                        imageUrl={proyecto.imageUrl}
+                        repoUrl={proyecto.repoUrl}
+                    />
                 ))}
             </div>
             {showButton && (
-            <button
-              onClick={scrollToTop}
-              className="fixed text-sm bottom-6 right-10 bg-white p-1 pl-2 pr-2 rounded-full text-black border border-black hover:bg-gray-950 hover:text-white transition duration-300"
-            >
-              ↑
-            </button>
+                <button
+                    onClick={scrollToTop}
+                    className="fixed text-sm bottom-6 right-10 bg-white p-1 pl-2 pr-2 rounded-full text-black border border-black hover:bg-gray-950 hover:text-white transition duration-300"
+                >
+                    ↑
+                </button>
             )}
         </div>
     );
