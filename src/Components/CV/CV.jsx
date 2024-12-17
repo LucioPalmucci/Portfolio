aca0b10d9e99f65febb1dfdb190e8806018e0278
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-12-17 11:14:55 -0300

    Fix pdf?

--- src/Components/CV/CV.jsx
+++ src/Components/CV/CV.jsx
@@ -1,4 +1,4 @@
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useContext } from 'react';
 import { LanguageContext } from '../../Context/LanguageContext';
@@ -6,15 +6,18 @@ import { LanguageContext } from '../../Context/LanguageContext';
 const CV = () => {
     const { isEnglish } = useContext(LanguageContext);
 
+    const curriculum = "/CV.pdf"
+    const resume = "/resume.pdf"
+
     return (
         <div className='justify-center items-center flex flex-col font-mono text-white mt-10 animate-slideDown'>
             <div className="text-start w-40 max-sm:w-2/3">
                 <a
-                    href={isEnglish ? '/resume.pdf' : '/CV.pdf'}
-                    download={isEnglish ? 'Lucio Palmucci Resume' : 'CV Lucio Palmucci'}
+                    href={isEnglish ? resume : curriculum}
+                    target="_blank" rel="noopener noreferrer"
                     className='text-lg rounded-full text-gray-400 bg-transparent border border-white hover:bg-black text-white py-2 px-4 mt-4 transition duration-500 ease-in-out inline-flex items-center'
                 >
-                    <FontAwesomeIcon icon={faDownload} className="mr-2" /> {isEnglish ? 'Download resume' : 'Descargar CV'}
+                    <FontAwesomeIcon icon={faEye} className="mr-2" /> {isEnglish ? 'View resume' : 'Ver CV'}
                 </a>
             </div>
         </div>
