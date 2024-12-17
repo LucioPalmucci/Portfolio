64b174e869f1d98b69e75f1bcbd8084dff383b46
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-12-16 21:16:49 -0300

    Download fix

--- src/Components/CV/CV.jsx
+++ src/Components/CV/CV.jsx
@@ -9,15 +9,16 @@ const CV = () => {
     return (
         <div className='justify-center items-center flex flex-col font-mono text-white mt-10 animate-slideDown'>
             <div className="text-start w-40 max-sm:w-2/3">
-                <button className='text-lg rounded-full text-gray-400 bg-transparent border border-white hover:bg-black text-white py-2 px-4 mt-4 transition duration-500'>
-                    <a href={isEnglish ? '/resume.pdf' : '/CV.pdf'} download={isEnglish ? 'Lucio Palmucci Resume' : 'CV Lucio Palmucci'}>
-                    <FontAwesomeIcon icon={faDownload}/> {isEnglish ? 'Download resume' : 'Descargar CV'}
-                    </a>
-                </button>
+                <a
+                    href={isEnglish ? '/resume.pdf' : '/CV.pdf'}
+                    download={isEnglish ? 'Lucio Palmucci Resume' : 'CV Lucio Palmucci'}
+                    className='text-lg rounded-full text-gray-400 bg-transparent border border-white hover:bg-black text-white py-2 px-4 mt-4 transition duration-500 ease-in-out inline-flex items-center'
+                >
+                    <FontAwesomeIcon icon={faDownload} className="mr-2" /> {isEnglish ? 'Download resume' : 'Descargar CV'}
+                </a>
             </div>
         </div>
     );
 };
 
 export default CV;
-
